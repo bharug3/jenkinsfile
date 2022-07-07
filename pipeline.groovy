8f24142b909020261f63d2ba71b8a376ed81942e
bharug3 <108127447+bharug3@users.noreply.github.com>
2022-07-08 00:41:20 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,24 +1,16 @@
 pipeline {
     agent any
-
     stages {
-        stage('Build') {
-            steps {
-                echo 'Building..'
-                sh"Hash=${git log -1}"
-                echo"Git Hash: ${Hash}"
-                sh"Branch=${git branch}"
-                echo"Git Branch : ${Branch}"
-            }
-        }
-        stage('Test') {
-            steps {
-                echo 'Testing..'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
+        stage ('Get Commit') {
+            steps{
+                script{
+                    try {
+                        treeMapData = git(branch: branchName, credentialsId: credential, url: "${gitLabServer}/${projectName}/${repo}.git")
+                        echo treeMapData["GIT_COMMIT"]
+                    } catch (Exception e) {
+                        echo "${e}"
+                    }
+                }
             }
         }
     }
